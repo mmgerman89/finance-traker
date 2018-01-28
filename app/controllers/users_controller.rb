a1898862c156b857ea4fe6714e40ef9c9f3c813f
Germán Martínez <mmgerman89@gmail.com>
2018-01-28 00:12:53 -0300

    agregar y quitar amigos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,33 @@ class UsersController < ApplicationController
     @user = current_user
     @user_stocks = current_user.stocks
   end
+
+  def my_friends
+    @friendships = current_user.friends
+  end
+
+  def search
+    if params[:search_param].blank?
+      flash.now[:danger] = "Debe ingresar un término de búsqueda"
+    else
+      @users = User.search(params[:search_param])
+      @users = current_user.except_current_user(@users)
+      flash.now[:danger] = "No hay usuarios con esos términos" if @users.blank?
+    end
+    respond_to do |format|
+      format.js { render partial: 'friends/result' }
+    end
+  end
+
+  def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+
+    if current_user.save
+      flash[:success] = "Amigo agregado"
+    else
+      flash[:danger] = "Hubo un problema al agregar amigo"
+    end
+    redirect_to my_friends_path
+  end
 end
